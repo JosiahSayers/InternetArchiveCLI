7903fa34a79e058920bda0e4911eb23ee165be3e
Josiah Sayers <josiah.sayers15@gmail.com>
2023-07-14 17:41:53 -0400

    fix for no specific files passed

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 
 if [[ $GLOB ]]; then glob=" --glob=$GLOB"; else glob=""; fi
 if [[ $EXCLUDE ]]; then exclude=" --exclude $EXCLUDE"; else exclude=""; fi
+if [[ $SPECIFIC_FILES ]]; then files=" $SPECIFIC_FILE"; else files=""; fi
 
-/ia download "$COLLECTION""$glob""$exclude" "$SPECIFIC_FILES"
+/ia download "$COLLECTION""$glob""$exclude""$files"
