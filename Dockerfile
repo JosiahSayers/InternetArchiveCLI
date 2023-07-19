94fddcfbbdaddbc6e5bdcc3f1984d14d5951dafd
Josiah Sayers <josiah.sayers15@gmail.com>
2023-07-19 17:05:37 -0400

    fix up run script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:latest
 RUN curl -LOs https://archive.org/download/ia-pex/ia
 RUN chmod +x ia
+RUN apt-get update && apt-get install -y tini
 COPY start.sh /
 WORKDIR /downloads
-CMD /start.sh
+ENTRYPOINT [ "tini", "--" ]
+CMD [ "/start.sh" ]
--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [[ $GLOB ]]; then glob=" --glob=$GLOB"; else glob=""; fi
-if [[ $EXCLUDE ]]; then exclude=" --exclude $EXCLUDE"; else exclude=""; fi
-if [[ $SPECIFIC_FILES ]]; then files=" $SPECIFIC_FILE"; else files=""; fi
+if [[ $GLOB ]]; then glob=" --glob=\"${GLOB}\""; else glob=""; fi
+if [[ $EXCLUDE ]]; then exclude=" --exclude \"${$EXCLUDE}\""; else exclude=""; fi
+if [[ $SPECIFIC_FILES ]]; then files=" \"{$SPECIFIC_FILE}\""; else files=""; fi
 
-/ia download "$COLLECTION""$glob""$exclude""$files"
+echo /ia download "$COLLECTION""$glob""$exclude""$files"
+
+sh -c "/ia download "$COLLECTION""$glob""$exclude""$files""
